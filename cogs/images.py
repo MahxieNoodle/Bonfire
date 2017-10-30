533a17bac5b2c3c0b36005beb92471baed0042b2
MahxieNoodle <MahxieNoodle@users.noreply.github.com>
2017-10-29 20:12:10 -0600

    Added blacklist message again

--- cogs/images.py
+++ cogs/images.py
@@ -240,10 +240,10 @@ class Images:
             rand_image = data[rand_image_number]['file_url']
             rand_image_tags = data[rand_image_number]['tags']
 
-            if not [i for e in blacklist for i in rand_image_tags.split(" ") if e in i]:
-                 await ctx.send(rand_image)
+            if  [i for e in blacklist for i in rand_image_tags.split(" ") if e in i]:
+                 await ctx.send("Sorry, there were no results with those tags or you used a blacklisted tag. {}".format(ctx.message.author.mention))
             else:
-                await ctx.send("Sorry, there were no results with those tags or you used a blacklisted tag. {}".format(ctx.message.author.mention))
+                await ctx.send(rand_image)
 
         except (ValueError, KeyError):
             await ctx.send("No results with that tag {}".format(ctx.message.author.mention))
