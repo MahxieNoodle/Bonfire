dada73482366a780576f0695dfe4560bc52e9dcf
unknown <mahx@mahxdesigns.com>
2018-02-27 10:56:50 -0700

    fixed typo

--- cogs/images.py
+++ cogs/images.py
@@ -268,8 +268,8 @@ class Images:
         try:
             retry = 0
             while (retry < 4):
-                rand_image = data[0]['file_url']
-                rand_image_tags = data[0]['tags']
+                rand_image = data[1]['file_url']
+                rand_image_tags = data[1]['tags']
                 if not [i for e in blacklist for i in rand_image_tags.split(" ") if e in i]:
                     await ctx.send(rand_image)
                     break
