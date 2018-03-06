eb9f28ef4bb93d3d86e601472ee69aed0ea23863
unknown <mahx@mahxdesigns.com>
2018-03-06 16:44:22 -0700

    Remove typo

--- cogs/filters.py
+++ cogs/filters.py
@@ -11,8 +11,7 @@ class Filters:
     def __init__(self, bot):
         self.bot = bot
 
-        @commands.command()
-        @commands.guild_only()
+        @commands.command(aliases=['blocklist'])
         @utils.custom_perms(send_messages=True)
         @utils.check_restricted()
         async def blacklists(self, ctx):
@@ -21,11 +20,8 @@ class Filters:
             EXAMPLE: !blacklists
             RESULT: All blacklists for this server server"""
             #tags = self.bot.db.load('filters', key=str(ctx.guild.id), pluck='filters')
-            if 1==1:
-                ctx.send("There are no tags setup on this server! :c")
-            else:
-                await ctx.send("There are no tags setup on this server!")
-                return
+            await ctx.send("There are no tags setup on this server!")
+
 
 
 
