ff478fc9f58f1af161ede94e20f172f562073079
unknown <mahx@mahxdesigns.com>
2018-03-06 16:28:49 -0700

    Changes to filters

--- cogs/filters.py
+++ cogs/filters.py
@@ -1,15 +1,17 @@
+import rethinkdb as r
 from discord.ext import commands
-import discord
+from discord.ext.commands.cooldowns import BucketType
 
 from . import utils
 
+import discord
 import asyncio
-
 
 class Filters:
 
     def __init__(self, bot):
         self.bot = bot
+        server = discord.guild.id
 
         @commands.command()
         @commands.guild_only()
@@ -20,17 +22,14 @@ class Filters:
 
             EXAMPLE: !blacklists
             RESULT: All blacklists for this server server"""
-            tags = self.bot.db.load('filters', key=server, pluck='filters')
+            tags = self.bot.db.load('filters', key=str(ctx.guild.id), pluck='filters')
             if tags:
                 entries = [t['trigger'] for t in blacklists]
                 pages = utils.Pages(self.bot, message=ctx.message, entries=entries)
                 await pages.paginate()
             else:
                 await ctx.send("There are no tags setup on this server!")
-
-
-            ##TODO finish later
-
+                return
 
 
 
