ccc6c5f9b65ed677b491c72e1f26e71278a40d4b
unknown <mahx@mahxdesigns.com>
2018-03-06 17:23:02 -0700

    test

--- cogs/filters.py
+++ cogs/filters.py
@@ -1,10 +1,10 @@
-import rethinkdb as r
 from discord.ext import commands
 import discord
 
 from . import utils
 
 import asyncio
+
 
 
 class Filters:
@@ -17,9 +17,18 @@ class Filters:
         @utils.custom_perms(send_messages=True)
         @utils.check_restricted()
         async def blacklists(self, ctx):
+            """Prints all blacklists and their tags for e621 and derpi.
 
-            await ctx.send("There are no tags setup on this server!")
-            return
+            EXAMPLE: !blacklists
+            RESULT: All blacklists for this server server"""
+            tags = self.bot.db.load('filters', key=ctx.guild.id, pluck='filters')
+            if tags:
+                entries = [t['trigger'] for t in blacklists]
+                pages = utils.Pages(self.bot, message=ctx.message, entries=entries)
+                await pages.paginate()
+            else:
+                await ctx.send("There are no tags setup on this server!")
+                return
 
 def setup(bot):
     bot.add_cog(Filters(bot))
