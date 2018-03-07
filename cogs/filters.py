8124f68f0eaa900d5ffd70f818044c294b60b994
unknown <mahx@mahxdesigns.com>
2018-03-06 17:11:03 -0700

    test

--- cogs/filters.py
+++ cogs/filters.py
@@ -1,35 +1,25 @@
 import rethinkdb as r
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
+import discord
 
 from . import utils
 
-import discord
 import asyncio
 
+
 class Filters:
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['blocklist'])
-    @commands.guild_only()
-    @utils.custom_perms(send_messages=True)
-    @utils.check_restricted()
-    async def blacklists(self, ctx, user = None):
-        """Makes me hug a person!
+        @commands.command()
+        @commands.guild_only()
+        @utils.custom_perms(send_messages=True)
+        @utils.check_restricted()
+        async def blacklists(self, ctx):
 
-        EXAMPLE: !hug @Someone
-        RESULT: I hug the shit out of that person"""
-        if user is None:
-            user = ctx.message.author
-        else:
-            converter = commands.converter.MemberConverter()
-            try:
-                user = await converter.convert(ctx, user)
-            except commands.converter.BadArgument:
-                await ctx.send("Error: Could not find user: {}".format(user))
-                return
-
+            await ctx.send("There are no tags setup on this server!")
+            return
 
 def setup(bot):
     bot.add_cog(Filters(bot))
