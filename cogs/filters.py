858aa196e1437ffe9a319a63e4f2670e5977357b
unknown <mahx@mahxdesigns.com>
2018-03-12 16:23:44 -0600

    Change filter add method

--- cogs/filters.py
+++ cogs/filters.py
@@ -12,6 +12,7 @@ class Filters:
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.command()
     @commands.guild_only()
     @utils.custom_perms(send_messages=True)
@@ -74,7 +75,7 @@ class Filters:
     @commands.guild_only()
     @utils.custom_perms(send_messages=True)
     @utils.check_restricted()
-    async def add_filter(self, ctx):
+    async def add_filter(self, ctx, *options):
         """Use this to add a new filter that can be used in this server
 
         EXAMPLE: !filter add
@@ -83,64 +84,47 @@ class Filters:
         def check(m):
             return m.channel == ctx.message.channel and m.author == ctx.message.author and len(m.content) > 0
 
-        my_msg = await ctx.send("Ready to setup a new filter! What do you want the filterName for the filter to be?")
+        if len(options) != 2:
+            await ctx.send("You need to provide 2-3 options! Such as \n `add derpi 49372` \n or \n `remove e621`")
+            return
+        else:
+            # Get the three arguments from this list, then make sure the 2nd is either from or to
+            if len(options) > 2:
+                arg3 = 0
+                return
+            else:
+                arg1, arg2, arg3 = options
 
-        try:
-            msg = await self.bot.wait_for("message", check=check, timeout=60)
-        except asyncio.TimeoutError:
-            await ctx.send("You took too long!")
-            return
+            if arg2.lower() not in ['derpi', 'e621']:
+                await ctx.send("The 2nd option needs to be either \"derpi\" or \"e621\". Such as: `add derpi 49372` "
+                               "or `add e621 tag,tag_name,tag`")
+                return
+            elif arg1.lower() not in ['add', 'remove']:
+                await ctx.send("The 2nd option needs to be either \"add\" or \"delete\". Such as: `add derpi 49372` "
+                               "or `remove e621`")
+                return
 
-        filterName = msg.content.lower().strip()
-        forbidden_filters = ['add', 'create', 'setup', 'edit', '']
-        if len(filterName) > 100:
-            await ctx.send("Please keep filter filterNames under 100 characters")
-            return
-        elif filterName in forbidden_filters:
-            await ctx.send(
-                "Sorry, but your filter filterName was detected to be forbidden. "
-                "Current forbidden filter filterNames are: \n{}".format("\n".join(forbidden_filters)))
-            return
+        await ctx.send("I have just restricted {} {} {}".format(arg1, arg2, arg3))
+
+        filterName = arg2.content.lower().strip()
+        filterString = arg3.strip()
 
         filters = self.bot.db.load('filters', key=ctx.message.guild.id, pluck='filters') or []
         if filters:
             for t in filters:
                 if t['filterName'].lower().strip() == filterName:
+                    # we need to edit the filter as it exists
                     await ctx.send("There is already a filter setup called {}!".format(filterName))
                     return
-
-        try:
-            await my_msg.delete()
-            await msg.delete()
-        except (discord.Forbidden, discord.HTTPException):
-            pass
-
-        if filterName.lower() in ['edit', 'delete', 'remove', 'stop']:
-            await ctx.send("You can't create a filter with {}!".format(filterName))
-            return
-
-        my_msg = await ctx.send(
-            "Alright, your new filter can be called with {}!\n\nWhat do you want to be displayed with this filter?".format(
-                filterName))
-
-        try:
-            msg = await self.bot.wait_for("message", check=check, timeout=60)
-        except asyncio.TimeoutError:
-            await ctx.send("You took too long!")
-            return
-
-        result = msg.content
-        try:
-            await my_msg.delete()
-            await msg.delete()
-        except (discord.Forbidden, discord.HTTPException):
-            pass
+                else:
+                #uhh
+                    return
 
         # The different DB settings
         filter = {
             'author': str(ctx.message.author.id),
             'filterName': filterName,
-            'result': result
+            'result': filterString
         }
         filters.append(filter)
         entry = {
@@ -149,6 +133,8 @@ class Filters:
         }
         self.bot.db.save('filters', entry)
         await ctx.send("I have just setup a new filter for this server! You can call your filter with {}".format(filterName))
+
+
 
     @filter.command(name='edit')
     @commands.guild_only()
@@ -224,6 +210,52 @@ class Filters:
                     return
         else:
             await ctx.send("There are no filters setup on this server!")
+
+
+
+    @commands.command()
+    @commands.guild_only()
+    @utils.custom_perms(manage_guild=True)
+    @utils.check_restricted()
+    async def filter(self, ctx, *options):
+        """
+        This is an intuitive command to restrict something to/from something
+        The format is `!restrict what from/to who/where`
+
+        For example, `!restrict command to role` will require a user to have `role`
+        to be able to run `command`
+        `!restrict command to channel` will only allow `command` to be ran in `channel`
+
+        EXAMPLE: !restrict boop from @user
+        RESULT: This user can no longer use the boop command
+        """
+        # First make sure we're given three options
+        if len(options) != 2:
+            await ctx.send("You need to provide 2-3 options! Such as \n `add derpi 49372` \n or \n `remove e621`")
+            return
+        else:
+            # Get the three arguments from this list, then make sure the 2nd is either from or to
+            if len(options) > 2:
+                arg3 = 0
+                return
+            else:
+                arg1, arg2, arg3 = options
+
+            if arg2.lower() not in ['derpi', 'e621']:
+                await ctx.send("The 2nd option needs to be either \"derpi\" or \"e621\". Such as: `add derpi 49372` "
+                               "or `add e621 tag,tag_name,tag`")
+                return
+            elif arg1.lower() not in ['add', 'remove']:
+                await ctx.send("The 2nd option needs to be either \"add\" or \"delete\". Such as: `add derpi 49372` "
+                               "or `remove e621`")
+                return
+
+        await ctx.send("I have just restricted {} {} {}".format(arg1, arg2, arg3))
+
+
+
+
+
 
 
 def setup(bot):
