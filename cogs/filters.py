a8e08d3511764a4df899379bfef7138a5e968f6b
unknown <mahx@mahxdesigns.com>
2018-03-12 16:30:36 -0600

    Change filter add logic error

--- cogs/filters.py
+++ cogs/filters.py
@@ -166,12 +166,12 @@ class Filters:
         RESULT: This user can no longer use the boop command
         """
         # First make sure we're given three options
-        if len(options) != 2:
+        if len(options) >= 2:
             await ctx.send("You need to provide 2-3 options! Such as \n `add derpi 49372` \n or \n `remove e621`")
             return
         else:
             # Get the three arguments from this list, then make sure the 2nd is either from or to
-            if len(options) > 2:
+            if len(options) < 2:
                 arg3 = 0
                 return
             else:
