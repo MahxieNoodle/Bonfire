6dc312a55eee559086f20c39ee778f3ee851ab8e
unknown <mahx@mahxdesigns.com>
2018-03-16 19:50:16 -0600

    if this works i will eat my socks

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot or utils.should_ignore(bot, message):
         return
-    react_photo(message)
+    message.send(react_photo(message))
     await bot.process_commands(message)
 
 @bot.event
